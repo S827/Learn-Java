3791b47fdadedfa587daa3a51300cbcacd2327e7
s827 <dskumar827@gmail.com>
2023-03-01 18:55:16 +0530

    changed directory

---
+++ Basics/HFJAVA/Basics/Main.java
@@ -0,0 +1,10 @@
+package Basics;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("jjjj");
+        System.out.println("jsdhjfs");
+    }
+}
---
+++ Basics/HFJAVA/Basics/MyFirstApp.java
@@ -0,0 +1,8 @@
+package Basics;
+
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+    }
+}
